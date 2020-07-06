1a639391b14adae580ff6e9e84fd8b2b55002531
Chris Bradley <chris.bradley@cy.id.au>
2020-07-06 22:32:04 +1000

    Fix syntax

--- terraform/aws_lambda.tf
+++ terraform/aws_lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda_function" {
   function_name = "hotham-alert-${var.environment}"
-  role          = aws_iam_role.iam_role.arn
+  role          = aws_iam_role.lambda_iam_role.arn
   handler       = "main.handler"
 
   s3_bucket = aws_s3_bucket.s3_bucket.id
